572b3457decb3f9118d2dcdcb4084d9f464eb45d
valpar <valmarparts@gmail.com>
2020-08-25 16:07:53 +0300

    sorting items, deploying items to db

--- webshop/webshop/src/app/item/item-list/item-list.component.ts
+++ webshop/webshop/src/app/item/item-list/item-list.component.ts
@@ -10,12 +10,37 @@ import { Item } from '../item.model';
 export class ItemListComponent implements OnInit {
   items: Item[]
   selectedCategory: string;
+  itemsShown: Item[];
 
   constructor(private itemService: ItemService) { }
 
   ngOnInit(): void {
     this.items = this.itemService.getItems();
+    this.onSortAll();
     console.log(this.items);
   }
 
+  onSortAll(){
+    this.itemsShown = this.items;
+  }
+
+  onSortTitleAsc(){
+    this.itemsShown = this.items.slice().sort((item, itemNext) => item.title.localeCompare(itemNext.title));
+  }
+
+  onSortTitleDesc(){
+    this.itemsShown = this.items.slice().sort((item, itemNext) => itemNext.title.localeCompare(item.title));
+  }
+
+  onSortPriceAsc(){
+    this.itemsShown = this.items.slice().sort(
+
+    );
+  }
+
+  onSortPriceDesc(){
+    this.itemsShown = this.items.slice().sort(
+
+    );
+  }
 }
--- webshop/webshop/src/app/item/item-new/item-new.component.ts
+++ webshop/webshop/src/app/item/item-new/item-new.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ToastService } from 'angular-toastify';
+import { TranslateService } from '@ngx-translate/core';
+
 import { ItemService } from '../item.service';
+import { Item } from '../item.model';
 
 @Component({
   selector: 'app-item-new',
@@ -9,16 +15,37 @@ import { ItemService } from '../item.service';
 })
 export class ItemNewComponent implements OnInit {
 
-  constructor(private itemService: ItemService) { }
+  constructor(
+    private itemService: ItemService,
+    private router: Router,
+    private toastService: ToastService,
+    private translate: TranslateService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
-    let newItem = form.value;
-    newItem.price = '$'+newItem.price
-    this.itemService.addItem(form.value);
+    // console.log(form.value);
+    // let newItem = form.value;
+    // newItem.price = '$'+newItem.price
+    // this.itemService.addItem(newItem);
+
+    let newItem = new Item(
+      form.value.imgSrc,
+      form.value.title,
+      form.value.price,
+      form.value.category,
+    )
+
+    this.itemService.addNewItem(newItem);
+
+    this.toastService.success(this.translate.instant('Item added'));
+
+    // this.router.navigate(['..'], {relativeTo: this.route});
+
+    setTimeout(()=>{
+      this.router.navigateByUrl('/');
+    }, 1500)
   }
 
 }
